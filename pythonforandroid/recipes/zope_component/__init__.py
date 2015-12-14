6fdec294613e5aa8e01067dc5c518d01fa70957c
Ryan Pessa <ryan@essential-elements.net>
2015-12-14 12:57:30 -0600

    add zope_component recipe

---
+++ pythonforandroid/recipes/zope_component/__init__.py
@@ -0,0 +1,15 @@
+
+from pythonforandroid.toolchain import PythonRecipe
+
+
+class ZopeComponentRecipe(PythonRecipe):
+	name = 'zope_component'
+	version = '4.2.2'
+	url = 'https://pypi.python.org/packages/source/z/zope.component/zope.component-{version}.tar.gz'
+	
+	depends = ['python2', 'setuptools', 'zope', 'zope_event']
+	
+	call_hostpython_via_targetpython = False
+
+
+recipe = ZopeComponentRecipe()
